40629a003eedf963aba2c492eb35021872ff44a8
Nghia Dep Trai <trongnghia280799@gmail.com>
2019-09-19 20:54:55 +0700

    heruko

--- src/app.js
+++ src/app.js
@@ -1,4 +1,39 @@
 
 const openStream = require('./openStream');
+const $ = require('jquery');
+const Peer = require('simple-peer');
+var wrtc = require('electron-webrtc');
+const playVideo = require('./playVideo');
+openStream((stream) => {
+    playVideo(stream, 'localStream');
+    // Peer to peer The sender and receiver Peer 
+    const peer = new Peer({
+        wrtc: wrtc,
+        initiator: location.hash === '#1',
+        trickle: false,
+        stream: stream
+    });
+    peer.on('signal', token => {
+        console.log(token);
+        $('#textMySignal').val(JSON.stringify(token));
 
-openStream();
+    });
+
+    $('#buttonSignal').click(() => {
+        // retrieve data from INPUT
+        const friendSignal = JSON.parse($('#textFriendSignal').val());
+        console.log(friendSignal);
+        // báo tín hiệu 
+        peer.signal(friendSignal);
+    });
+
+    peer.on('stream', (stream) => {
+        const video = document.getElementById('friendStream');
+        // Older browsers may not have srcObject
+        video.srcObject = stream;
+        video.onloadedmetadata = () => {
+            video.play();
+        };
+    });
+
+});
--- src/openStream.js
+++ src/openStream.js
@@ -2,7 +2,7 @@ const $ = require('jquery');
 const Peer = require('simple-peer');
 var wrtc = require('electron-webrtc');
 const playVideo = require('./playVideo');
-function openStream() {
+function openStream(cb) {
   //The MediaDevices.getUserMedia() method prompts the user for permission to use a media input 
   if (navigator.mediaDevices === undefined) {
     navigator.mediaDevices = {};
@@ -23,37 +23,7 @@ function openStream() {
 
   navigator.mediaDevices.getUserMedia({ audio: false, video: true })
     .then((stream) => {
-      playVideo(stream,'localStream');
-      // Peer to peer The sender and receiver Peer 
-      const peer = new Peer({
-        wrtc: wrtc,
-        initiator: location.hash === '#1',
-        trickle: false,
-        stream: stream
-      });
-      peer.on('signal', token => {
-        console.log(token);
-        $('#textMySignal').val(JSON.stringify(token));
-
-      });
-
-      $('#buttonSignal').click(() => {
-        // retrieve data from INPUT
-        const friendSignal = JSON.parse($('#textFriendSignal').val());
-        console.log(friendSignal);
-        // báo tín hiệu 
-        peer.signal(friendSignal);
-      });
-
-      peer.on('stream',(stream) => {
-        const video = document.getElementById('friendStream');
-        // Older browsers may not have srcObject
-        video.srcObject = stream;
-        video.onloadedmetadata = () => {
-            video.play();
-        };
-      });
-     
+      cb(stream);
     })
     .catch(function (err) {
       console.log(err.name + ": " + err.message);
